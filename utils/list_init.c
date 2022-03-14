4296d65c832de93be9b80b18c7a52df44be592dd
Yujung Gil <74306759+fairyroad@users.noreply.github.com>
2022-03-15 00:10:43 +0900

    Create list_init.c

---
+++ utils/list_init.c
@@ -0,0 +1,14 @@
+#include "../push_swap.h"
+
+t_stack	*list_init(void)
+{
+	t_stack	*tmp;
+
+	tmp = (t_stack *) malloc(sizeof(t_stack));
+	if (tmp == NULL)
+		return (NULL);
+	tmp->first = NULL;
+	tmp->last = NULL;
+	tmp->size = 0;
+	return (tmp);
+}
