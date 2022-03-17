beafb9cfd53591368601c117d4b606d2a83a6c56
fairy_road <kuj9628@naver.com>
2022-03-17 18:59:37 +0900

    Edit push_swap.h

--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: ygil <ygil@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/10 18:23:23 by ygil              #+#    #+#             */
-/*   Updated: 2022/03/17 18:36:30 by ygil             ###   ########.fr       */
+/*   Updated: 2022/03/17 18:45:35 by ygil             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,38 +40,39 @@ typedef struct s_tab
 	int		size;
 }	t_tab;
 
-t_tab	convert_to_tab(t_stack	*a);
-int		check_integer(char	*str);
-int		check_duplicate(t_tab	*tab);
-int		check_sorted(t_tab	*tab);
-void	format_stack(t_stack	*a);
-void	sort_smallsize(t_stack	*a, t_stack	*b);
-void	sort_bigsize(t_stack *a, t_stack *b);
-int		find_index(t_tab *tab, int val);
-void	sort_tab(t_tab *tab);
-int		ft_isdigit(int c);
-int		ft_isspace(int c);
+t_tab		convert_to_tab(t_stack	*a);
+int			check_integer(char	*str);
+int			check_duplicate(t_tab	*tab);
+int			check_sorted(t_tab	*tab);
+void		format_stack(t_stack	*a);
+void		sort_smallsize(t_stack	*a, t_stack	*b);
+void		sort_bigsize(t_stack *a, t_stack *b);
+int			find_index(t_tab *tab, int val);
+void		sort_tab(t_tab *tab);
+int			ft_isdigit(int c);
+int			ft_isspace(int c);
 
-size_t	ft_strlcpy(char *dest, const char *src, size_t size);
-int		ft_atoi(const char *str);
-char	**ft_split(char const *str, char c);
-void	list_add_front(t_stack *a, int val);
-void	list_add_last(t_stack *a, int content);
-void	list_remove(t_stack *a, t_node *node, void (*del)(void*));
-void	list_clear(t_stack *a, void (*del)(void*));
-t_node	*list_create_node(void *content);
-t_stack	*list_init(void);
+size_t		ft_strlcpy(char *dest, const char *src, size_t size);
+int			ft_atoi(const char *str);
+char		**ft_split(char const *str, char c);
+void		list_add_front(t_stack *a, int val);
+void		list_add_last(t_stack *a, int content);
+void		list_remove(t_stack *a, t_node *node, void (*del)(void*));
+void		list_clear(t_stack *a, void (*del)(void*));
+t_node		*list_create_node(void *content);
+t_stack		*list_init(void);
 
-void	pa(t_stack *a, t_stack *b);
-void	pb(t_stack *a, t_stack *b);
-void	ra(t_stack *a);
-void	rb(t_stack *b);
-void	rr(t_stack *a, t_stack *b);
-void	rra(t_stack *a);
-void	rrb(t_stack *b);
-void	rrr(t_stack *a, t_stack *b);
-void	sa(t_stack *a);
-void	sb(t_stack *b);
-void	ss(t_stack *a, t_stack *b);
+void		pa(t_stack *a, t_stack *b);
+void		pb(t_stack *a, t_stack *b);
+void		ra(t_stack *a);
+void		rb(t_stack *b);
+void		rr(t_stack *a, t_stack *b);
+void		rra(t_stack *a);
+void		rrb(t_stack *b);
+void		rrr(t_stack *a, t_stack *b);
+void		sa(t_stack *a);
+void		sb(t_stack *b);
+void		ss(t_stack *a, t_stack *b);
+static void	parse_arguments(int argc, char **argv, t_stack *a, t_stack *b);
 
 #endif
